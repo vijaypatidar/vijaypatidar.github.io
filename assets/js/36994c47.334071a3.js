e05af52a7ab2227f54e9b3a134f46697a7a4ce66
Vijay Patidar <vramotiya@gmail.com>
2024-08-22 18:49:33 +0530

    Updated portfolio

--- assets/js/36994c47.334071a3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[9858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- assets/js/a7456010.cb3f5629.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_portfolio=self.webpackChunkmy_portfolio||[]).push([[1235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
