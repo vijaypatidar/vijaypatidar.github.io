9aad25348c39bd0d8240221eeeb434c2d0b0b67b
vijaypatidar <vramotiya+github@gmail.com>
2024-11-14 14:16:06 +0530

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
-npm i && npm run build
-git clone git@github.com:vijaypatidar/vijaypatidar.github.io.git 
-cd vijaypatidar.github.io
-rm -rf ./*           
-cd ../build
-ll
-cp -r . ../vijaypatidar.github.io
-cd ../vijaypatidar.github.io
-git add .
-git commit -m "Updated portfolio"
-git push
-cd ..
-sudo rm -r vijaypatidar.github.io
+npm i
+npm run build
+npm run deploy-gh
+echo "Application deployed"
